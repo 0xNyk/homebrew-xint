adde75a33403cb8a31e27e35d12869421d614c7a
Nyk <0xnykcd@googlemail.com>
2026-02-19 22:41:31 +0700

    feat(formula): use prebuilt macOS arm64 binary assets

--- Formula/xint.rb
+++ Formula/xint.rb
@@ -1,15 +1,24 @@
 class Xint < Formula
   desc "X Intelligence CLI"
   homepage "https://github.com/0xNyk/xint-rs"
-  url "https://github.com/0xNyk/xint-rs/archive/refs/tags/2026.2.19.6.tar.gz"
-  sha256 "16a27ce57d481f43b8a7fb446fdb201b31c53653a374be836403d71232cafbd2"
   license "MIT"
 
-  depends_on "rust" => :build
+  if OS.mac? && Hardware::CPU.arm?
+    url "https://github.com/0xNyk/xint-rs/releases/download/2026.2.19.6/xint-rs-macos-arm64-2026.2.19.6.tar.gz"
+    sha256 "df7aed1adefe0311d41cec1136e0b1af720e0611c487af09f13625919521d933"
+  else
+    url "https://github.com/0xNyk/xint-rs/archive/refs/tags/2026.2.19.6.tar.gz"
+    sha256 "16a27ce57d481f43b8a7fb446fdb201b31c53653a374be836403d71232cafbd2"
+    depends_on "rust" => :build
+  end
   conflicts_with "xint-rs", because: "both install the xint executable"
 
   def install
-    system "cargo", "install", *std_cargo_args(path: ".")
+    if OS.mac? && Hardware::CPU.arm?
+      bin.install "xint"
+    else
+      system "cargo", "install", *std_cargo_args(path: ".")
+    end
   end
 
   test do
