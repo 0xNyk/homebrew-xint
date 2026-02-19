21cb950c22e7e89563ac4830795fe660f6675ae8
Nyk <0xnykcd@googlemail.com>
2026-02-19 22:43:19 +0700

    fix(formula): align macOS binary checksums with release assets

--- Formula/xint.rb
+++ Formula/xint.rb
@@ -5,7 +5,7 @@ class Xint < Formula
 
   if OS.mac? && Hardware::CPU.arm?
     url "https://github.com/0xNyk/xint-rs/releases/download/2026.2.19.6/xint-rs-macos-arm64-2026.2.19.6.tar.gz"
-    sha256 "df7aed1adefe0311d41cec1136e0b1af720e0611c487af09f13625919521d933"
+    sha256 "4423090c859298baf57c4282b2a969aafe1c057e51869c4a870e3cc75805a71a"
   else
     url "https://github.com/0xNyk/xint-rs/archive/refs/tags/2026.2.19.6.tar.gz"
     sha256 "16a27ce57d481f43b8a7fb446fdb201b31c53653a374be836403d71232cafbd2"
