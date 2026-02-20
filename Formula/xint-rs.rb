7e5e99f8f8c167d9add28311585b65b9d9bcce9c
Nyk <0xnykcd@googlemail.com>
2026-02-20 23:43:35 +0700

    chore(release): bump formulas to v2026.2.20.2

--- Formula/xint-rs.rb
+++ Formula/xint-rs.rb
@@ -4,11 +4,11 @@ class XintRs < Formula
   license "MIT"
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/0xNyk/xint-rs/releases/download/2026.2.19.7/xint-rs-macos-arm64-2026.2.19.7.tar.gz"
-    sha256 "69ccad109461be08ceb54073270a3c1c185b69081fc6de1614a5f34dc46b8bc0"
+    url "https://github.com/0xNyk/xint-rs/releases/download/2026.2.20.2/xint-rs-macos-arm64-2026.2.20.2.tar.gz"
+    sha256 "5e39443786746cd05fa1fe85947003116870d43e58ed9e6e6b871ece580c26ab"
   else
-    url "https://github.com/0xNyk/xint-rs/archive/refs/tags/2026.2.19.7.tar.gz"
-    sha256 "dd9de50efdaac9b877a8085d277ff47dcf09fca4abbaccf16571b0df7a8afd8f"
+    url "https://github.com/0xNyk/xint-rs/archive/refs/tags/2026.2.20.2.tar.gz"
+    sha256 "ee3d44b4a988fc7aee4c0158e70c3c6464912cfadb322477161c5a33c1cf2a02"
     depends_on "rust" => :build
   end
   conflicts_with "xint", because: "both install the xint executable"
